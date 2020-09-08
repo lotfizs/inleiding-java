325d6c40a97d3db065e4b2954f8cdaad6167de2b
lzsnation <2133349@talnet.com>
2020-09-08 15:14:49 +0200

    plan 1

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Hallo lotfi zaher saidi 170");
+        System.out.println("Hallo lotfi zaher saidi 17");
     }
 }
