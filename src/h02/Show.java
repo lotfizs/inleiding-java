9df609a6c287c2f12a9002b59d198f2003fb73df
lzsnation <2133349@talnet.com>
2020-09-08 20:41:27 +0200

    plan 1

--- src/h02/Show.java
+++ src/h02/Show.java
@@ -16,16 +16,14 @@ Voorbeeld 2.1
 import java.awt.*;
 import java.applet.*;
 
-//een klasse met de naam Show van het type Applet
 public class Show extends Applet {
 
-    //een (lege) methode die de Applet initialiseert
     public void init() {
-
+        setBackground(Color.blue);
     }
-    //een methode die de inhoud van het scherm tekent
+
     public void paint(Graphics g) {
-        g.drawString("Welcome to Java!!", 50, 60 );
+        g.setColor(Color.yellow);
+        g.drawString("Lotfi Zaher Saidi", 50, 60 );
     }
-
 }
