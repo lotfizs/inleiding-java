b633bacbb7088a047ccc3a660c2092029c1fc27d
lzsnation <2133349@talnet.com>
2020-09-09 12:34:59 +0200

    plan 1

--- src/h02/Show.java
+++ src/h02/Show.java
@@ -1,29 +1,25 @@
 package h02;
 
-/*
 
-@(#)Show.java 1.0 03/02/26
-
-*
-
-Voorbeeld 2.1
-
-*
-
-*/
-
-//importeer de benodigde klassen uit de Java bibliotheek
 import java.awt.*;
 import java.applet.*;
 
 public class Show extends Applet {
 
     public void init() {
-        setBackground(Color.blue);
     }
 
     public void paint(Graphics g) {
+        setBackground(Color.blue);
         g.setColor(Color.yellow);
-        g.drawString("Lotfi Zaher Saidi", 50, 60 );
+        g.drawLine(20, 20, 60, 70);
+        g.drawLine(60, 70, 100, 20);
+        g.drawLine(20, 20, 100, 20);
+        g.drawRect(150,100,100,100);
+        g.drawLine(150,100,200,50);
+        g.drawLine(250,100,200,50);
+        g.drawRect(185,160,30,40);
+        g.drawRect(160,120,30,20);
+        g.drawRect(210,120,30,20);
     }
-}
+    }
