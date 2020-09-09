548e8fa1cd72e7969dfa6404cc8a6b072e732452
lzsnation <2133349@talnet.com>
2020-09-09 13:44:44 +0200

    plan 1

---
+++ src/h04/elips.java
@@ -0,0 +1,18 @@
+package h04;
+
+import java.awt.*;
+import java.applet.*;
+
+public class elips extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+    setBackground(Color.blue);
+    g.setColor(Color.yellow);
+    g.fillArc(20,20,200,400,90,360);
+
+    }
+}
+
