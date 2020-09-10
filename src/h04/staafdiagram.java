6bbeff251baad94440acc6e64a61306eedb65e1c
lzsnation <2133349@talnet.com>
2020-09-10 12:48:47 +0200

    plan 1

--- src/h04/staafdiagram.java
+++ src/h04/staafdiagram.java
@@ -9,46 +9,62 @@ public class staafdiagram extends Applet {
     }
 
     public void paint(Graphics g) {
+        setBackground(Color.white);
+        //Namen
         g.setColor(Color.black);
-        //kilogram
-        g.drawString("100kg", 10, 50);
-        g.drawString("80kg",10,75);
-        g.drawString("60",10,100);
-        g.drawString("40",10,125);
-        g.drawString("20",10,150);
-        g.drawString("0",10,175);
+        g.drawString("Valerie",50,260);
+        g.drawString("Hans",115,260);
+        g.drawString("Jeroen",170,260);
 
-        // stefan
-        //valerie
-        g.setColor(Color.pink);
-        g.fillRect(113,125,80,50);
+
+        //De verdeling
+        g.setColor(Color.BLACK);
+        g.drawString("0 kg",16,240);
+
+        g.drawString("20 kg",9,220);
+
+        g.drawString("40 kg",9,200);
+
+        g.drawString("60 kg",9,180);
+
+        g.drawString("80 kg",9,160);
+
+        g.drawString("100 kg",2,140);
+
+        g.drawString("120 kg",2,120);
+
+        g.drawString("140 kg",2,100);
+
+        g.drawString("160 kg",2,80);
+
+        g.drawString("180 kg",2,60);
+
+        g.drawString("200 kg",2,40);
+
+        //De achtergrond rectangle
         g.setColor(Color.black);
-        g.drawString("valerie",130,190);
-        //hans
-        g.setColor(Color.darkGray);
-        g.fillRect(184,75,66,100);
-        g.setColor(Color.black);
-        g.drawString("hans",204,190);
-        //jeroen
-        g.setColor(Color.blue);
-        g.fillRect(50,50,66,125);
-        g.setColor(Color.black);
-        g.drawString("jeroen",60,190);
+        g.drawRect(40,40,180,200);
 
-        //lijnen
-        g.setColor(Color.black);
-        g.drawRect(50,50,200,75);
-        g.drawRect(50,50,200,25);
-        g.drawRect(50,50,200,50);
-        g.drawRect(50,50,200,75);
+        //De stafen
+        g.setColor(Color.yellow);
+        g.fillRect(40,200,60,40);
 
+        g.setColor(Color.magenta);
+        g.fillRect(100,160,60,80);
 
+        g.setColor(Color.green);
+        g.fillRect(160,140,60,100);
 
-
-
-
-
-
-
+        //De lijnen
+        g.setColor(Color.BLACK);
+        g.drawLine(40,60,220,60);
+        g.drawLine(40,80,220,80);
+        g.drawLine(40,100,220,100);
+        g.drawLine(40,120,220,120);
+        g.drawLine(40,140,220,140);
+        g.drawLine(40,160,220,160);
+        g.drawLine(40,180,220,180);
+        g.drawLine(40,200,220,200);
+        g.drawLine(40,220,220,220);
     }
 }
