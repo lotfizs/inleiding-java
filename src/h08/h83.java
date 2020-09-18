ea1ae1b0c5150a586da6588e2d98d9b40ab97663
lzsnation <2133349@talnet.com>
2020-09-18 12:40:39 +0200

    plan 1

--- src/h08/h83.java
+++ src/h08/h83.java
@@ -9,24 +9,46 @@ public class h83 extends Applet {
     TextField tekstvak;
     Label label;
     double getal;
+    Button knop;
+    double btw;
+    String s;
 
     public void init() {
         tekstvak = new TextField("", 20);
         label = new Label("Type een getal");
-        tekstvak.addActionListener( new TekstvakListener() );
+        tekstvak.addActionListener(new TekstvakListener());
         add(label);
         add(tekstvak);
+
+        knop = new Button("knop + 21%");
+        knop.addActionListener(new knopListener());
+        add(knop);
+
+        btw = 0.21;
+
+
     }
 
     public void paint(Graphics g) {
-        g.drawString("Het getal is " + getal, 50, 60 );
+        g.drawString("Het getal is " + getal, 50, 60);
     }
 
     class TekstvakListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             String s = tekstvak.getText();
-            getal = Double.parseDouble( s );
+            getal = Double.parseDouble(s);
             repaint();
         }
     }
-}
+            class knopListener implements ActionListener {
+                public void actionPerformed(ActionEvent e) {
+                    s = tekstvak.getText();
+                    getal = Double.parseDouble( s );
+                    getal = getal * btw + getal;
+                    repaint();
+
+                }
+            }
+        }
+
+
