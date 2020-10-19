34c5d10eb8402cf135f308b35580d256b3b551e8
lzsnation <2133349@talnet.com>
2020-10-19 12:14:05 +0200

    plan 1

---
+++ src/h111/h114.java
@@ -0,0 +1,20 @@
+package h111;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class h114 extends Applet {
+
+    public void paint(Graphics g) {
+        int Getal = 0;
+        int x = 10;
+        int Tafel;
+
+        while (Getal < 10) {
+            x += 15;
+            Getal++;
+            Tafel = 3 * Getal;
+            g.drawString("" + Tafel,x,10);
+        }
+    }
+}
